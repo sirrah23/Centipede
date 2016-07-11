7c5e275580968af6accdae6d6f28e23231884aab
Sirrah23 <hseth93@gmail.com>
2016-07-10 20:09:35 -0400

    Collision with wall makes centipede segment rotate

--- segment.py
+++ segment.py
@@ -10,18 +10,17 @@ class Segment:
         self.rect.y = pos[1]
         self.screenWidth = width
         self.screenHeight = height
-        #Start with moving down
+        # Start with moving down
         self.verticalDirection = 10
-        #How fast do the centipede segments move left/right
-        self.horizontalSpeed = 2
+        # How fast do the centipede segments move left/right
+        self.horizontalSpeed = 1
 
     def computeNewSpeed(self):
         #If the centipede is just moved up or down then reset that
         if self.speed[1] != 0:
             return [self.speed[0],0]
-
         newSpeed = []
-        #If centipede hits left wall, start moving right 
+        #If centipede hits left wall, start moving right
         if self.rect.left <= 0:
             newSpeed=[self.horizontalSpeed,self.verticalDirection]
         #If centipede hits right wall, start moving left
@@ -31,7 +30,6 @@ class Segment:
         # If centipede hits top or bottom then start going the other way
         if (self.rect.top <= 0 and self.verticalDirection < 0) or (self.rect.bottom >= self.screenHeight and self.verticalDirection > 0):
             self.verticalDirection *= -1
-
         #Return the current speed of the centipede
         if newSpeed == []:
             return self.speed
@@ -43,8 +41,22 @@ class Segment:
 
     def move(self):
         newSpeed = self.computeNewSpeed()
+        # We changed directions
+        if newSpeed[0] != self.speed[0]:
+            self.rotate()
         self.updateSpeed(newSpeed)
         self.rect = self.rect.move(self.speed)
+
+    # After a centipede segment hits the wall it needs to "turn around" in order to go the other way.
+    # This method rotates the rect object in the appropriate direction.
+    def rotate(self):
+        oldCenter = self.rect.center
+        # degrees * direction
+        angle = 180
+        self.image = pygame.transform.rotate(self.image, angle)
+        # Get new rect from rotated image
+        self.rect = self.image.get_rect()
+        self.rect.center = oldCenter
 
     def withinScreen(self):
         return True
